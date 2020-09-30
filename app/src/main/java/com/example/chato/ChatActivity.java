336589e9abdffbea4816dd5d49ce7af7390d785e
PavicevicMarko <33724686+PavicevicMarko@users.noreply.github.com>
2020-09-30 13:03:55 +0200

    pošiljanje medij in shranjevanje v bazo (brez prikaza)

--- app/src/main/java/com/example/chato/ChatActivity.java
+++ app/src/main/java/com/example/chato/ChatActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -16,13 +17,20 @@ import android.widget.EditText;
 import com.example.chato.Chat.MediaAdapter;
 import com.example.chato.Chat.Message;
 import com.example.chato.Chat.MessageAdapter;
+import com.example.chato.Chat.Pogovor;
 import com.example.chato.User.Uporabnik;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,8 +43,10 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager nChatListLayoutManager, mMediaLayoutManager;
 
     ArrayList<Message> messageList = new ArrayList<>();
+
     String chatID;
     DatabaseReference chatDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,7 +90,6 @@ public class ChatActivity extends AppCompatActivity {
                         creatorID = snapshot.child("creator").getValue().toString();   //vrne pošitelja iz baze
                     }
 
-
                     Message message = new Message(snapshot.getKey(), creatorID, text);  //sporočilo
                     messageList.add(message);
                     nChatListLayoutManager.scrollToPosition(messageList.size()-1);      // scrolla na zadnji element v chatu
@@ -105,21 +114,61 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
+    int totalMediaUpload = 0;
+    ArrayList<String> mediaIdList = new ArrayList<>();
+    EditText mMessage;
     private void sendMessage(){
-        EditText mMessage = findViewById(R.id.writeMessage);
+            mMessage = findViewById(R.id.writeMessage);
 
-        if(!mMessage.getText().toString().isEmpty()){
-            DatabaseReference msgDB = chatDB.push();
-            Map msgMap = new HashMap<>();
-            msgMap.put("text", mMessage.getText().toString());
+            String messageID = chatDB.push().getKey();
+            final DatabaseReference msgDB = chatDB.child(messageID);
+
+            final Map msgMap = new HashMap<>();
             msgMap.put("creator", FirebaseAuth.getInstance().getUid());
+            if(!mMessage.getText().toString().isEmpty()){
+                msgMap.put("text", mMessage.getText().toString());
+            }
 
-            msgDB.updateChildren(msgMap); //gor da v tabelo novo sporočilo
-         }
-        mMessage.setText(null);
+            if(!mediaUriList.isEmpty()){
+                for(final String mediaUri : mediaUriList){                                          //gre skozi medije v listi
+                    String mediaId = msgDB.child("media").push().getKey();
+                    mediaIdList.add(mediaId);
+                    final StorageReference filePath = FirebaseStorage.getInstance().getReference().child("chat").child(chatID).child(messageID).child(mediaId);
+
+                    UploadTask upload = filePath.putFile(Uri.parse(mediaUri));                              //cilj uploada
+
+                    upload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    msgMap.put("/media/" + mediaIdList.get(totalMediaUpload) +"/", uri.toString());
+
+                                    totalMediaUpload++;
+                                    if(totalMediaUpload == mediaUriList.size()){
+                                        updateDatabaseWithNewMessage(msgDB, msgMap);                          //upload v cloud
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            }else{
+            if(!mMessage.getText().toString().isEmpty()){
+                updateDatabaseWithNewMessage(msgDB, msgMap);
+                }
+            }
+
     }
 
-
+    private void updateDatabaseWithNewMessage(DatabaseReference newMsgDb, Map newMsgMap){               //shrani in zbriše
+        newMsgDb.updateChildren(newMsgMap);
+        mMessage.setText(null);
+        mediaUriList.clear();
+        mediaIdList.clear();
+        mMediaAdapter.notifyDataSetChanged();
+    }
 
 
     int PICK_IMAGE_INTENT = 1;
@@ -140,7 +189,7 @@ public class ChatActivity extends AppCompatActivity {
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 //        intent.setType("image/");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); //več naenkrat
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Izberi sliko"), PICK_IMAGE_INTENT);
 
